a81671c0696d1db94ccfebe0d0ee22c36cce63ac
khawlah-kh <44741544+khawlah-kh@users.noreply.github.com>
2021-09-13 13:52:27 +0300

    Initial Commit

---
+++ ViewModifir/ViewModifirApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModifirApp.swift
+//  ViewModifir
+//
+//  Created by khawlah khalid on 13/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct ViewModifirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
