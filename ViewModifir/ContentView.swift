60ad17021cd4768b28019bf906730cb71773e681
khawlah-kh <44741544+khawlah-kh@users.noreply.github.com>
2021-09-13 14:03:58 +0300

    Update ContentView.swift

--- ViewModifir/ContentView.swift
+++ ViewModifir/ContentView.swift
@@ -9,13 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        VStack{
         Text("Hello, world!")
+            .modifier(BrandStyle())
+            
+            
             .padding()
+        
+        
+        Image(systemName: "sun.min.fill")
+            .modifier(BrandStyle())
+            
+            
+            
+            
+            
+        }
+         
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+
+struct BrandStyle : ViewModifier{
+    
+    
+    func body(content:Content)->some View{
+        
+        
+        content
+            .font(.system(size: 32,weight: .bold))
+            .lineLimit(1)
+            .minimumScaleFactor(0.75)
+        
     }
 }
